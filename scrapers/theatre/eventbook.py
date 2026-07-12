96d41a57f5887facb4696e345c8e7f5f9a6d5d3a
Andrei-Mihai Nicolae <andrei@nicolaeandrei.com>
2026-07-12 13:48:15 +0300

    Add Eventbook theatre adapter

---
+++ scrapers/theatre/eventbook.py
@@ -0,0 +1,8 @@
+from models import Event
+from scrapers.music.eventbook import BUCHAREST_URL as EVENTS_URL
+from scrapers.music.eventbook import scrape as scrape_eventbook
+
+
+def scrape() -> list[Event]:
+    """Fetch Bucharest theatre events from Eventbook."""
+    return [event for event in scrape_eventbook() if event.category == "theatre"]
